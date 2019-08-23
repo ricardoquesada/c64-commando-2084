236805ea8e1c68eb388d641b2a76665183d56f9e
Ricardo Quesada <ricardoquesada@gmail.com>
2019-08-22 20:43:07 -0700

    add .asm and binaries fro co2.prg
    co2.asm generates exactly the same co2.prg as the original one

--- Makefile
+++ Makefile
@@ -5,20 +5,23 @@ X64 = x64
 X64SC = x64sc
 C1541 = c1541
 
-all: clean co1.prg d64 run
+all: clean d64 run
 
 co1.prg: src/main.asm src/music.asm src/level_data.asm
-	64tass -Wall --cbm-prg -o bin/co1.prg -L bin/list.txt -l bin/labels.txt --vice-labels src/main.asm
+	64tass -Wall --cbm-prg -o bin/co1.prg -L bin/list-co1.txt -l bin/labels.txt --vice-labels src/main.asm
 	md5sum bin/co1.prg orig/co1.prg
 
-d64: co1.prg
+co2.prg: src/co2.asm src/charset1-charset.bin src/charset2-charset.bin
+	64tass -Wall --cbm-prg -o bin/co2.prg -L bin/list-co2.txt src/co2.asm
+	md5sum bin/co2.prg orig/co2.prg
+
+d64: co1.prg co2.prg
 	$(C1541) -format "commando 2084,rq" d64 $(D64_IMAGE)
 	$(C1541) $(D64_IMAGE) -write orig/commando.prg "commando"
 	$(C1541) $(D64_IMAGE) -write bin/co1.prg co1
-	$(C1541) $(D64_IMAGE) -write orig/co2.prg co2
+	$(C1541) $(D64_IMAGE) -write bin/co2.prg co2
 	$(C1541) $(D64_IMAGE) -write orig/co3.prg co3
 	$(C1541) $(D64_IMAGE) -list
-
 
 run: d64
 	$(X64) -verbose -moncommands bin/labels.txt $(D64_IMAGE)
---
+++ src/co2.asm
@@ -0,0 +1,110 @@
+
+        * = $E000
+
+        .BINARY "charset1-charset.bin"
+        .BINARY "charset2-charset.bin"
+
+        * = $F000
+
+        .BYTE $05,$00,$15,$00,$05,$00,$15,$00
+        .BYTE $39,$00
+
+        LDA $DC0E    ;CIA1: CIA Control Register A
+        AND #$FE     ;#%11111110
+        STA $DC0E    ;CIA1: CIA Control Register A
+        LDA $01
+        AND #$FB     ;#%11111011
+        STA $01
+        LDX #$00     ;#%00000000
+        LDA $E000,X
+        STA $D000,X  ;Sprite 0 X Pos
+        LDA $E100,X
+        STA $D100,X
+        LDA $E200,X
+        STA $D200,X
+        LDA $E300,X
+        STA $D300,X
+        LDA $E400,X
+        STA $D400,X  ;Voice 1: Frequency Control - Low-Byte
+        LDA $E500,X
+        STA $D500,X
+        LDA $E600,X
+        STA $D600,X
+        LDA $E700,X
+        STA $D700,X
+        LDA $E800,X
+        STA $D800,X
+        LDA $E900,X
+        STA $D900,X
+        LDA $EA00,X
+        STA $DA00,X
+        LDA $EB00,X
+        STA $DB00,X
+        LDA $EC00,X
+        STA $DC00,X  ;CIA1: Data Port Register A
+        LDA $ED00,X
+        STA $DD00,X  ;CIA2: Data Port Register A
+        LDA $EE00,X
+        STA $DE00,X
+        LDA $EF00,X
+        STA $DF00,X
+        INX
+        BNE $F01A
+        LDA $01
+        ORA #$04     ;#%00000100
+        STA $01
+        LDA $DC0E    ;CIA1: CIA Control Register A
+        ORA #$01     ;#%00000001
+        STA $DC0E    ;CIA1: CIA Control Register A
+        RTS
+
+        ; Unused most probably
+
+        .BYTE $DC,$29,$FE
+
+        STA $DC0E    ;CIA1: CIA Control Register A
+        LDA $01
+        AND #$FB     ;#%11111011
+        STA $01
+        LDX #$00     ;#%00000000
+        LDA $E000,X
+        STA $D000,X  ;Sprite 0 X Pos
+        LDA $E100,X
+        STA $D100,X
+        LDA $E200,X
+        STA $D200,X
+        LDA $E300,X
+        STA $D300,X
+        LDA $E400,X
+        STA $D400,X  ;Voice 1: Frequency Control - Low-Byte
+        LDA $E500,X
+        STA $D500,X
+        LDA $E600,X
+        STA $D600,X
+        LDA $E700,X
+        STA $D700,X
+        LDA $E800,X
+        STA $D800,X
+        LDA $E900,X
+        STA $D900,X
+        LDA $EA00,X
+        STA $DA00,X
+        LDA $EB00,X
+        STA $DB00,X
+        LDA $EC00,X
+        STA $DC00,X  ;CIA1: Data Port Register A
+        LDA $ED00,X
+        STA $DD00,X  ;CIA2: Data Port Register A
+        LDA $EE00,X
+        STA $DE00,X
+        LDA $EF00,X
+        STA $DF00,X
+        INX
+        BNE $F09A
+        LDA $01
+        ORA #$04     ;#%00000100
+        STA $01
+        LDA $DC0E    ;CIA1: CIA Control Register A
+        ORA #$01     ;#%00000001
+
+        .BYTE $8D,$0E
